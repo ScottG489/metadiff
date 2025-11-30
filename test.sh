b5c3aa8782558edca009601423b612b39dd5476a
Scott Giminiani <scottg489@gmail.com>
2025-11-30 00:10:23 -0500

    Fix test.sh to support run task and branches

--- test.sh
+++ test.sh
@@ -10,12 +10,15 @@ cleanup() {
 
 declare ID_RSA_CONTENTS_BASE64
 declare DOCKER_CONFIG_CONTENTS_BASE64
+declare GIT_BRANCH
 
 # Change the location of these files based on where they are on your system
 ID_RSA_CONTENTS_BASE64=$(base64 ~/.ssh/id_rsa | tr -d '\n') ;
 DOCKER_CONFIG_CONTENTS_BASE64=$(base64 ~/.docker/config.json | tr -d '\n') ;
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 [[ -n $ID_RSA_CONTENTS_BASE64 ]]
 [[ -n $DOCKER_CONFIG_CONTENTS_BASE64 ]]
+[[ -n $GIT_BRANCH ]]
 
 # The local fs is mounted into the container and as such any files it writes will have their permissions changed.
 #   This will change the permissions back and clean up other files we don't want hanging around.
@@ -25,4 +28,4 @@ docker build infra/build -t metadiff-build-test && \
   docker run -it \
   --runtime=sysbox-runc \
   --volume "$PWD:/opt/build/metadiff" \
-  metadiff-build-test '{"ID_RSA": "'"$ID_RSA_CONTENTS_BASE64"'", "DOCKER_CONFIG": "'"$DOCKER_CONFIG_CONTENTS_BASE64"'"}'
+  metadiff-build-test '{"RUN_TASK": "test", "GIT_BRANCH": "'"$GIT_BRANCH"'", "ID_RSA": "'"$ID_RSA_CONTENTS_BASE64"'", "DOCKER_CONFIG": "'"$DOCKER_CONFIG_CONTENTS_BASE64"'"}'
